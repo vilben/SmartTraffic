1e5f59534b91a45d5190eb3fc5dedda96632f5f1
Benny <benny.villiger@eps.ch>
2022-03-03 22:42:14 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import sumolib
+#
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+
+net = sumolib.net.readNet()
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
