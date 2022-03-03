966dbaab1a156bc1529171b9633864a57d6913fc
Smokejoe <38937469+Smokejoe@users.noreply.github.com>
2022-03-03 22:40:05 +0100

    Actually do steps (#2)
    * run sumo gui from python code
    
    * steps
    
    Co-authored-by: Benny <benny.villiger@eps.ch>

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ sumoBinary = sumolib.checkBinary("sumo-gui")
 cmd = [sumoBinary, "-c", "config/hello.sumocfg"]
 
 traci.start(cmd)
+
+step = 0
+while step < 1000:
+    traci.simulationStep()
+    # whatever... tutorial stuff
+    # if traci.inductionloop.getLastStepVehicleNumber("0") > 0:
+    #     traci.trafficlight.setRedYellowGreenState("0", "GrGr")
+    step += 1
+
+traci.close()
+
