19bc68c1c150a421f2459878b63526da44e5b237
Moritz Schiesser <moritzschiesser@gmail.com>
2022-03-04 17:35:01 +0100

    actually fix vehType script

--- tools/vehicleTypeChanger.py
+++ tools/vehicleTypeChanger.py
@@ -33,19 +33,19 @@ def parseXml(inputFile):
 
 def addHardcodedVTypesWithReallyUglyHack():
     XML = """
-    <vType id="normal_car" vClass="passenger" maxSpeed="40" speedFactor="0.9" speedDev="0.2" sigma="0.5" color="white" guiShape="passenger" />
-    <vType id="sporty_car" vClass="passenger" maxSpeed="60" speedFactor="1.3" speedDev="0.1" sigma="0.1" color="red" guiShape="passenger/sedan" />
-    <vType id="trailer" vClass="trailer" maxSpeed="30" speedFactor="1" speedDev="0.05" color="gray" guiShape="truck/trailer" />
-    <vType id="coach" vClass="coach" maxSpeed="30" speedFactor="1" speedDev="0.05" color="green" guiShape="bus/coach" />
-    </routes>
+    <routes xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://sumo.dlr.de/xsd/routes_file.xsd">
+        <vType id="normal_car" vClass="passenger" maxSpeed="40" speedFactor="0.9" speedDev="0.2" sigma="0.5" color="white" guiShape="passenger" />
+        <vType id="sporty_car" vClass="passenger" maxSpeed="60" speedFactor="1.3" speedDev="0.1" sigma="0.1" color="red" guiShape="passenger/sedan" />
+        <vType id="trailer" vClass="trailer" maxSpeed="30" speedFactor="1" speedDev="0.05" color="gray" guiShape="truck/trailer" />
+        <vType id="coach" vClass="coach" maxSpeed="30" speedFactor="1" speedDev="0.05" color="green" guiShape="bus/coach" />
     """
 
     # read all lines from INPUT file
     with open(INPUT, "r") as f:
         lines = f.readlines()
         # remove last line
-        lines.pop()
-        lines.append(XML)
+        lines.pop(0)
+        lines.insert(0, XML)
         # write all lines to OUTPUT file
         with open(OUTPUT, "w") as f:
             f.writelines(lines)
