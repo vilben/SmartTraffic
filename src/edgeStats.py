db6cd37bac212d0853e37a3e8182430416a8b8f3
Moritz Schiesser <moritzschiesser@gmail.com>
2022-03-05 14:58:45 +0100

    fix exceptions import

--- src/edgeStats.py
+++ src/edgeStats.py
@@ -2,7 +2,7 @@ import errno
 import json
 import os
 import requests
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 import traci
